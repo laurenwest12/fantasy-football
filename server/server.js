bf93259254bb3a74ec530cd31937cdbb0ef6ee2f
Lauren West <laurenwest12@gmail.com>
2023-03-15 14:30:55 -0400

    Added connections to sleeper to get draft and player data

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 
 const port = process.env.PORT || 3001;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, async () => {
+  console.log(`Server running on port ${port}`);
+});
---
+++ server/sleeper.js
@@ -0,0 +1,77 @@
+const axios = require('axios');
+
+const getUserDrafts = async () => {
+  try {
+    const { data } = await axios.get(
+      `https://api.sleeper.app/v1/user/${process.env.USER_ID}/drafts/nfl/2022`
+    );
+    return data;
+  } catch (err) {
+    return {
+      statusCode: 404,
+      err: err?.message,
+    };
+  }
+};
+
+const getUserDraftByLeagueName = async (league) => {
+  try {
+    const { data } = await axios.get(
+      `https://api.sleeper.app/v1/user/${process.env.USER_ID}/drafts/nfl/2022`
+    );
+    return data.filter(({ metadata }) => metadata.name === league);
+  } catch (err) {
+    return {
+      statusCode: 404,
+      err: err?.message,
+    };
+  }
+};
+
+const getDraftPicks = async (leagueId) => {
+  try {
+    const { data } = await axios.get(
+      `https://api.sleeper.app/v1/draft/${leagueId}/picks`
+    );
+    return data;
+  } catch (err) {
+    return {
+      statusCode: err?.response?.status || 404,
+      err: err?.message,
+    };
+  }
+};
+
+const getPlayers = async () => {
+  const { data } = await axios.get(`https://api.sleeper.app/v1/players/nfl`);
+  const players = [];
+  for (let playerId in data) {
+    const playerData = data[playerId];
+    const { position, active } = playerData;
+    if (process.env.POSITIONS.includes(position) && active) {
+      players.push({
+        ...playerData,
+        personal_tier: 0,
+        ringer_tier: 0,
+        fp_tier: 0,
+        personal_pos_tier: 0,
+        ringer_pos_tier: 0,
+        fp_pos_tier: 0,
+        bye: 0,
+        adp: 0,
+        ecr: 0,
+        personal_ranking: 0,
+        ringer_ranking: 0,
+        fp_ranking: 0,
+      });
+    }
+  }
+  return players;
+};
+
+module.exports = {
+  getUserDrafts,
+  getUserDraftByLeagueName,
+  getDraftPicks,
+  getPlayers,
+};
