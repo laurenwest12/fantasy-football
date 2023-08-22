13fc120afd1f0645887a452626855bc510166675
Lauren West <laurenwest12@gmail.com>
2023-08-22 16:29:19 -0400

    rendering each position in tiers and added ability to expand and collapse

--- src/components/Tiers.js
+++ src/components/Tiers.js
@@ -1,23 +1,51 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 const Tiers = ({ players }) => {
+  const [expandedTiers, setExpandedTiers] = useState({});
+
+  const toggleTierExpansion = (position, tier) => {
+    setExpandedTiers((prevExpandedTiers) => {
+      const key = `${position}-${tier}`;
+      return { ...prevExpandedTiers, [key]: !prevExpandedTiers[key] };
+    });
+  };
+
   return (
-    <div>
-      <div>QB</div>
-      <div>
-        {Object.entries(players.QB).map(([tier, tierPlayers]) => (
+    <div className="tiers__container">
+      {Object.keys(players).map((position) => (
+        <div className="tier__position" key={position}>
+          <div className="tier__header">{position}</div>
           <div>
-            <div>TIER {tier}</div>
-            <div>
-              {tierPlayers.map((player) => (
-                <div>
-                  <div>{player.full_name}</div>
+            {Object.entries(players[position]).map(([tier, tierPlayers]) => (
+              <div className="tier__container" key={tier}>
+                <div>TIER {tier}</div>
+                <div className="tier__players">
+                  {tierPlayers
+                    .slice(
+                      0,
+                      expandedTiers[`${position}-${tier}`] ? undefined : 10
+                    )
+                    .map((player) => (
+                      <div className="tier__player" key={player.id}>
+                        <div>{player.full_name}</div>
+                      </div>
+                    ))}
                 </div>
-              ))}
-            </div>
+                {tierPlayers.length > 10 && (
+                  <button
+                    className="expand-button"
+                    onClick={() => toggleTierExpansion(position, tier)}
+                  >
+                    {expandedTiers[`${position}-${tier}`]
+                      ? 'Collapse'
+                      : 'Expand'}
+                  </button>
+                )}
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
     </div>
   );
 };
